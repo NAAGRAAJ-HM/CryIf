d2ea5a41ad50d41173c7738ec52811df17c503f5
root <nagaraja.hm.0001@gmail.com>
2022-01-18 10:49:45 +0100

    Update: submodule CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -5,3 +5,48 @@ class_CryIf CryIf;
 FUNC(void, CRYIF_CODE) class_CryIf::InitFunction(void){
 }
 
+FUNC(void, CRYIF_CODE) class_CryIf::GetVersionInfo(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::ProcessJob(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::CancelJob(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyElementSet(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeySetValid(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyElementGet(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyElementCopy(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyElementCopyPartial(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyCopy(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::RandomSeed(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyGenerate(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyDerive(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyExchangeCalcPubVal(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::KeyExchangeCalcSecret(void){
+}
+
+FUNC(void, CRYIF_CODE) class_CryIf::CbNotification(void){
+}
+
--- LATEST/CryIf.h
+++ LATEST/CryIf.h
@@ -5,7 +5,22 @@
 
 class class_CryIf{
    public:
-      FUNC(void, CRYIF_CODE) InitFunction(void);
+      FUNC(void, CRYIF_CODE) InitFunction          (void);
+      FUNC(void, CRYIF_CODE) GetVersionInfo        (void);
+      FUNC(void, CRYIF_CODE) ProcessJob            (void);
+      FUNC(void, CRYIF_CODE) CancelJob             (void);
+      FUNC(void, CRYIF_CODE) KeyElementSet         (void);
+      FUNC(void, CRYIF_CODE) KeySetValid           (void);
+      FUNC(void, CRYIF_CODE) KeyElementGet         (void);
+      FUNC(void, CRYIF_CODE) KeyElementCopy        (void);
+      FUNC(void, CRYIF_CODE) KeyElementCopyPartial (void);
+      FUNC(void, CRYIF_CODE) KeyCopy               (void);
+      FUNC(void, CRYIF_CODE) RandomSeed            (void);
+      FUNC(void, CRYIF_CODE) KeyGenerate           (void);
+      FUNC(void, CRYIF_CODE) KeyDerive             (void);
+      FUNC(void, CRYIF_CODE) KeyExchangeCalcPubVal (void);
+      FUNC(void, CRYIF_CODE) KeyExchangeCalcSecret (void);
+      FUNC(void, CRYIF_CODE) CbNotification        (void);
 };
 
 extern class_CryIf CryIf;
