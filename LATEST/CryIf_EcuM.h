06ceae9d1083fa354980788419e4859bec99a193
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:06:19 +0100

    Update: submodule CryIf

--- LATEST/CryIf_EcuM.h
+++ LATEST/CryIf_EcuM.h
@@ -27,8 +27,8 @@ class interface_CryIf_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, CRYIF_CODE) InitFunction   (void);
-      FUNC(void, CRYIF_CODE) DeInitFunction (void);
+      virtual FUNC(void, CRYIF_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, CRYIF_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
