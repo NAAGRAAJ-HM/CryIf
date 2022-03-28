da3c712096ed6da2dc7a88ace2a7a6b68eec2898
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:46:28 +0200

    Update: AUTOSAR basic software ECU abstraction module CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -90,7 +90,23 @@ FUNC(void, CRYIF_CODE) module_CryIf::GetVersionInfo(void){
 FUNC(void, CRYIF_CODE) module_CryIf::MainFunction(void){
 }
 
-#include "CryIf_Unused.hpp"
+class class_CryIf_Unused{
+   public:
+      FUNC(void, CRYIF_CODE) ProcessJob            (void);
+      FUNC(void, CRYIF_CODE) CancelJob             (void);
+      FUNC(void, CRYIF_CODE) KeyElementSet         (void);
+      FUNC(void, CRYIF_CODE) KeySetValid           (void);
+      FUNC(void, CRYIF_CODE) KeyElementGet         (void);
+      FUNC(void, CRYIF_CODE) KeyElementCopy        (void);
+      FUNC(void, CRYIF_CODE) KeyElementCopyPartial (void);
+      FUNC(void, CRYIF_CODE) KeyCopy               (void);
+      FUNC(void, CRYIF_CODE) RandomSeed            (void);
+      FUNC(void, CRYIF_CODE) KeyGenerate           (void);
+      FUNC(void, CRYIF_CODE) KeyDerive             (void);
+      FUNC(void, CRYIF_CODE) KeyExchangeCalcPubVal (void);
+      FUNC(void, CRYIF_CODE) KeyExchangeCalcSecret (void);
+      FUNC(void, CRYIF_CODE) CbNotification        (void);
+};
 
 FUNC(void, CRYIF_CODE) class_CryIf_Unused::ProcessJob(void){
 }
