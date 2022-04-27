ec53a9e90162246cd15ffbeb7fb8cba04b5c2872
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-27 10:43:26 +0000

    Update: AUTOSAR basic software ECU abstraction module CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -31,24 +31,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class class_CryIf_Functionality{
-   public:
-      FUNC(void, CRYIF_CODE) ProcessJob            (void);
-      FUNC(void, CRYIF_CODE) CancelJob             (void);
-      FUNC(void, CRYIF_CODE) KeyElementSet         (void);
-      FUNC(void, CRYIF_CODE) KeySetValid           (void);
-      FUNC(void, CRYIF_CODE) KeyElementGet         (void);
-      FUNC(void, CRYIF_CODE) KeyElementCopy        (void);
-      FUNC(void, CRYIF_CODE) KeyElementCopyPartial (void);
-      FUNC(void, CRYIF_CODE) KeyCopy               (void);
-      FUNC(void, CRYIF_CODE) RandomSeed            (void);
-      FUNC(void, CRYIF_CODE) KeyGenerate           (void);
-      FUNC(void, CRYIF_CODE) KeyDerive             (void);
-      FUNC(void, CRYIF_CODE) KeyExchangeCalcPubVal (void);
-      FUNC(void, CRYIF_CODE) KeyExchangeCalcSecret (void);
-      FUNC(void, CRYIF_CODE) CbNotification        (void);
-};
-
 class module_CryIf:
       public abstract_module
    ,  public class_CryIf_Functionality
