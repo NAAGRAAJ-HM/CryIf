314da03c38acbab745331fee54b7b6079be02e8c
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:00 +0000

    Update: AUTOSAR basic software ECU abstraction module CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -113,7 +113,7 @@ FUNC(void, CRYIF_CODE) module_CryIf::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  CRYIF_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, CRYIF_CODE) module_CryIf::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  CRYIF_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, CRYIF_CODE) module_CryIf::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  CRYIF_E_UNINIT
       );
 #endif
    }
