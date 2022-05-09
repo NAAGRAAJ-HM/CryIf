9a807a6eca486838f6407677e43ebcead509e950
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:34:46 +0000

    Update: AUTOSAR basic software ECU abstraction module CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -35,6 +35,13 @@ class module_CryIf:
       public abstract_module
    ,  public class_CryIf_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_CryIf(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
