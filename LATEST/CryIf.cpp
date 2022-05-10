64a14424f3c9498e46ac9708450bf4d29b7972d9
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-10 10:08:12 +0000

    Update: AUTOSAR basic software ECU abstraction module CryIf

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -89,7 +89,10 @@ FUNC(void, CRYIF_CODE) module_CryIf::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, CRYIF_CONFIG_DATA, CRYIF_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == CryIf_InitCheck)
-   if(E_OK != IsInitDone){
+   if(
+         E_OK
+      != IsInitDone
+   ){
 #endif
       if(NULL_PTR != lptrCfgModule){
          if(STD_HIGH){
@@ -127,7 +130,10 @@ FUNC(void, CRYIF_CODE) module_CryIf::InitFunction(
 
 FUNC(void, CRYIF_CODE) module_CryIf::DeInitFunction(void){
 #if(STD_ON == CryIf_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == CryIf_InitCheck)
       IsInitDone = E_NOT_OK;
@@ -147,7 +153,10 @@ FUNC(void, CRYIF_CODE) module_CryIf::DeInitFunction(void){
 
 FUNC(void, CRYIF_CODE) module_CryIf::MainFunction(void){
 #if(STD_ON == CryIf_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == CryIf_InitCheck)
    }
