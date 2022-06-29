8817535aa297ca90afb24e97535f980c5063dddd
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-29 09:01:34 +0000

    Update: AUTOSAR basic software ECU abstraction module for crypto stack - interface layer

--- LATEST/CryIf.cpp
+++ LATEST/CryIf.cpp
@@ -41,12 +41,8 @@ class module_CryIf:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-            Std_TypeReturn          IsInitDone{E_NOT_OK};
-      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
 
    public:
-      module_CryIf(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
-      }
       FUNC(void, CRYIF_CODE) InitFunction(
          CONSTP2CONST(CfgModule_TypeAbstract, CRYIF_CONFIG_DATA, CRYIF_APPL_CONST) lptrCfgModule
       );
@@ -71,18 +67,7 @@ CONSTP2VAR(infSchMClient, CRYIF_VAR, CRYIF_CONST) gptrinfSchMClient_CryIf = &Cry
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_CryIf, CRYIF_VAR) CryIf(
-   {
-         CRYIF_AR_RELEASE_VERSION_MAJOR
-      ,  CRYIF_AR_RELEASE_VERSION_MINOR
-      ,  0x00
-      ,  0xFF
-      ,  0x01
-      ,  '0'
-      ,  '1'
-      ,  '0'
-   }
-);
+VAR(module_CryIf, CRYIF_VAR) CryIf;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
