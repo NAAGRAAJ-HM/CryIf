83ad0d01c6a7b1622343577debfebaca29287034
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:05:04 +0200

    Update: AUTOSAR basic software ECU abstraction module for crypto stack - interface layer

--- VirtualECU_LATEST/EcuabCryIf.cpp
+++ VirtualECU_LATEST/EcuabCryIf.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabCryIf.cpp                                                         */
+/* File   : EcuabCryIf.cpp                                                    */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABCRYIF_AR_RELEASE_VERSION_MAJOR                                         4
-#define ECUABCRYIF_AR_RELEASE_VERSION_MINOR                                         3
+#define ECUABCRYIF_AR_RELEASE_VERSION_MAJOR                                    4
+#define ECUABCRYIF_AR_RELEASE_VERSION_MINOR                                    3
 
 /******************************************************************************/
 /* MACROS                                                                     */
