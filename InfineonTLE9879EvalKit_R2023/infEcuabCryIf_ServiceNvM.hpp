449d0acbd776918d91ac5804b3bd4f493c86240e
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:01 +0100

    Update: AUTOSAR basic software ECU abstraction module for crypto stack - interface layer

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabCryIf_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabCryIf_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABCRYIF_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTECUABCRYIF_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstEcuabCryIf                                                                              \
+   ,  {CONSTECUABCRYIF_AR_RELEASE_VERSION_MAJOR, CONSTECUABCRYIF_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
